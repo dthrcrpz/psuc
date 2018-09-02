16fe220c9363c3748853c4710f0e938148d0acfb
Deither Corpuz <dthrcrpz@gmail.com>
2018-09-02 14:48:12 +0800

    message after success submission done

---
+++ server/app/Http/Controllers/ComplaintController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Complaint;
+use Illuminate\Support\Facades\Validator;
+
+class ComplaintController extends Controller
+{
+    public function store(Request $r){
+    	// $this->validate($r, [
+    	// 	'ip' => 'unique:complaints'
+    	// ]);
+
+    	$action = Complaint::create([
+    		'way' => $r->way,
+    		'about' => $r->about,
+    		'target' => $r->target,
+            'message' => $r->message,
+    		'ip' => $r->ip,
+    	]);
+    	return $action;
+    }
+}
---
+++ server/database/migrations/2018_09_02_054524_create_complaints_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComplaintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ip');
+            $table->string('way');
+            $table->string('about');
+            $table->string('target');
+            $table->text('message');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complaints');
+    }
+}
