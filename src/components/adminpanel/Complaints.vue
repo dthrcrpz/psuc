e2d7b21bae912ac75318222f48171fe5f90eba07
teryong <dthrcrpz@gmail.com>
2018-10-03 21:59:10 +0800

    admin panel complaints message filter

--- src/components/adminpanel/Complaints.vue
+++ src/components/adminpanel/Complaints.vue
@@ -4,14 +4,14 @@
 			<thead>
 				<tr>
 					<th>Show on Public?</th>
-					<th class="sortable" @click="sort(2)">Name <i class="fa fa-sort" aria-hidden="true"></i></th>
-					<th class="sortable" @click="sort(3)">Alias <i class="fa fa-sort" aria-hidden="true"></i></th>
-					<th class="sortable" @click="sort(4)">IP <i class="fa fa-sort" aria-hidden="true"></i></th>
-					<th class="sortable" @click="sort(5)">What <i class="fa fa-sort" aria-hidden="true"></i></th>
-					<th class="sortable" @click="sort(6)">About <i class="fa fa-sort" aria-hidden="true"></i></th>
-					<th class="sortable" @click="sort(7)">Target <i class="fa fa-sort" aria-hidden="true"></i></th>
-					<th class="sortable" @click="sort(8)">Date Submitted <i class="fa fa-sort" aria-hidden="true"></i></th>
-					<th class="sortable" @click="sort(9)">Message <i class="fa fa-sort" aria-hidden="true"></i></th>
+					<th class="sortable" @click="sort(1)">Submitted By <i class="fa fa-sort" aria-hidden="true"></i></th>
+					<th class="sortable" @click="sort(2)">Alias <i class="fa fa-sort" aria-hidden="true"></i></th>
+					<th class="sortable" @click="sort(3)">What <i class="fa fa-sort" aria-hidden="true"></i></th>
+					<th class="sortable" @click="sort(4)">About <i class="fa fa-sort" aria-hidden="true"></i></th>
+					<th class="sortable" @click="sort(5)">Target <i class="fa fa-sort" aria-hidden="true"></i></th>
+					<th class="sortable" @click="sort(6)">Date Submitted <i class="fa fa-sort" aria-hidden="true"></i></th>
+					<th class="sortable" @click="sort(7)">Date Last Updated <i class="fa fa-sort" aria-hidden="true"></i></th>
+					<th class="sortable" @click="sort(8)">Message <i class="fa fa-sort" aria-hidden="true"></i></th>
 					<th>Actions</th>
 				</tr>
 			</thead>
@@ -25,31 +25,12 @@
 					</td>
 					<td>{{ c.name }}</td>
 					<td>{{ c.alias }}</td>
-					<td>{{ c.ip }}</td>
 					<td>{{ c.way }}</td>
 					<td>{{ c.about }}</td>
 					<td>{{ c.target }}</td>
 					<td>{{ c.created_at }}</td>
-					<td>{{ c.message }}</td>
-					<td>
-						<button><i class="fa fa-trash" aria-hidden="true"></i> Delete</button>
-					</td>
-				</tr>
-				<tr class="complaints-item" v-for="c in complaints">
-					<td>
-						<label class="switch">
-							<input type="checkbox" :checked="c.show == 1">
-							<span class="slider round"></span>
-						</label>
-					</td>
-					<td>{{ c.name }}</td>
-					<td>{{ c.alias }}</td>
-					<td>{{ c.ip }}</td>
-					<td>{{ c.way }}</td>
-					<td>{{ c.about }}</td>
-					<td>{{ c.target }}</td>
-					<td>{{ c.created_at }}</td>
-					<td>{{ c.message }}</td>
+					<td>{{ c.updated_at }}</td>
+					<td>{{ c.message | str_limit }}</td>
 					<td>
 						<button><i class="fa fa-trash" aria-hidden="true"></i> Delete</button>
 					</td>
@@ -67,6 +48,12 @@
 				complaints: []
 			}
 		},
+		filters: {
+			str_limit(str){
+				let x = str.substr(0, 20) + '...'
+				return x
+			}
+		},
 		methods: {
 			sort(target){
 				sortHTML('#complaints-table','.complaints-item', 'td:nth-child('+target+')')
@@ -74,10 +61,10 @@
 		},
 		mounted(){
 			let me = this
-			me.$parent.isLoading = 1
+			me.$parent.$parent.isLoading = 1
 			me.$binding('complaints', db.collection('complaints'))
 			.then(() => {
-				me.$parent.isLoading = 0
+				me.$parent.$parent.isLoading = 0
 			})
 		}
 	}
