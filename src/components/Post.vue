a6451476a610c8dc7e759ef199ef416bfb9c7ade
teryong <dthrcrpz@gmail.com>
2018-10-11 21:35:04 +0800

    started the comment sections for posts

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,33 +1,39 @@
 <template>
-	<div class="post-template">
-		<div class="col left">
-			<div class="info-group">
-				<p class="label">Posted by:</p>
-				<p class="value">{{ data.alias }}</p>
+	<div class="post-template-wrapper">
+		<div class="post-template">
+			<div class="col left">
+				<div class="info-group">
+					<p class="label">Posted by:</p>
+					<p class="value">{{ data.alias }}</p>
+				</div>
+				<div class="info-group">
+					<p class="label">Status:</p>
+					<p class="value">{{ data.way }}</p>
+				</div>
+				<div class="info-group">
+					<p class="label">Subject:</p>
+					<p class="value">{{ data.about }}</p>
+				</div>
+				<div class="info-group">
+					<p class="label">About:</p>
+					<p class="value">{{ data.target }}</p>
+				</div>
+				<div class="info-group">
+					<p class="label">Date Submitted:</p>
+					<p class="value">{{ data.created_at | moment("MMM DD, YYYY | hh:mm a") }}</p>
+				</div>
 			</div>
-			<div class="info-group">
-				<p class="label">Status:</p>
-				<p class="value">{{ data.way }}</p>
+			<div class="col right">
+				<div class="info-group">
+					<p class="value">{{ data.message }}</p>
+				</div>
 			</div>
-			<div class="info-group">
-				<p class="label">Subject:</p>
-				<p class="value">{{ data.about }}</p>
-			</div>
-			<div class="info-group">
-				<p class="label">About:</p>
-				<p class="value">{{ data.target }}</p>
-			</div>
-			<div class="info-group">
-				<p class="label">Date Submitted:</p>
-				<p class="value">{{ data.created_at | moment("MMM DD, YYYY | hh:mm a") }}</p>
-			</div>
+			<button class="delete" @click="confirmDelete(data['.key'])" v-if="data.user_id == $parent.$parent.decodedClientToken.user_id || $parent.$parent.isAdminLoggedIn"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
 		</div>
-		<div class="col right">
-			<div class="info-group">
-				<p class="value">{{ data.message }}</p>
-			</div>
+		<div class="post-comments">
+			<button class="add">Add Comment</button>
+			<button class="view">View Comments</button>
 		</div>
-		<button class="delete" @click="confirmDelete(data['.key'])" v-if="data.user_id == $parent.$parent.decodedClientToken.user_id"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
 	</div>
 </template>
 
